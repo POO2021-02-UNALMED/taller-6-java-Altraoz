e335b947866ee302025d7c130b89091a442196ef
Altraoz <curresty@unal.edu.co>
2021-11-30 19:33:59 -0500

    3

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,8 +1,12 @@
 package vehiculos;
 
+import java.util.ArrayList;
+
 public class Fabricante {
     private String nombre;
     private Pais pais;
+    private static ArrayList<Fabricante> fabricas = new ArrayList<Fabricante>();
+    public int vehiculosFabricados;
 
     public Fabricante(String nombre, Pais pais){
         this.nombre = nombre;
@@ -22,4 +26,23 @@ public class Fabricante {
         this.pais = pais;
     }
 
+    public void vehiculoFabricado () {
+        vehiculosFabricados ++;
+    }
+    public int getVehiculosFabricados() {
+        return vehiculosFabricados;
+    }
+    public void setVehiculosFabricados(int vehiculosFabricados) {
+        this.vehiculosFabricados = vehiculosFabricados;
+    }
+
+    public static Fabricante fabricaMayorVentas() {
+        Fabricante mayoresVentas = fabricas.get(0);
+        for (int i = 1; i<fabricas.size(); i++) {
+            if (fabricas.get(i).getVehiculosFabricados() > mayoresVentas.getVehiculosFabricados()) {
+                mayoresVentas = fabricas.get(i);
+            }
+        }
+        return mayoresVentas;
+    }
 }
--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -21,6 +21,7 @@ public class Vehiculo {
         this.traccion = traccion;
         this.fabricante = fabricante;
         n_vehiculos ++;
+
     }
 
     public String getPlaca(){
